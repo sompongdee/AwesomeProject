7b4f7c2be310010f86bef90788072a1bc58dc28f
sompongdee <166291287+sompongdee@users.noreply.github.com>
2024-04-07 16:26:14 +0700

    Initial commit

---
+++ screens/week2/Ex10.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function Ex10() {
+  return (
+    <View style={{ flex : 1 ,flexDirection : 'row' , alignItems : 'center' , alignItems : 'stretch'}}>
+   
+      <View style={{ flex : 1 , backgroundColor : '#4A90E2' }}></View>
+        
+    </View>
+  );
+}
