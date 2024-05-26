db6260d4289b951f4fa3ae752cbac6f9034a5adf
sompongdee <166291287+sompongdee@users.noreply.github.com>
2024-05-26 11:59:54 +0700

    week10

--- components/week6/HomeIconMenu.js
+++ components/week6/HomeIconMenu.js
@@ -25,9 +25,8 @@ export default function HomeIconMenu() {
       <MyIcon title="Pokemon" name="bolt" size={30} color="orange" onPress={()=>{ navigation.navigate("PokemonTab"); }} />
       
       <MyIcon title="Book Store" name="cubes" size={30} color="orange" onPress={ ()=>{ navigation.navigate("Book")}} />
+      <MyIcon title="Location" name="map-marker" size={30} color="orange" onPress={ ()=>{ navigation.navigate("Location") }}  />
 
-        <MyIcon title="xxx" name="question" size={30} color="orange" />
-        <MyIcon title="xxx" name="question" size={30} color="orange" />
         <MyIcon title="xxx" name="question" size={30} color="orange" />
         <MyIcon title="More" name="ellipsis-h" size={30} color="orange" />
       </View>
---
+++ screens/week10/LocationQuiz.js
@@ -0,0 +1,51 @@
+import { useNavigation } from "@react-navigation/native";
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import GPS from "../../helpers/GPS";
+import MyMapView from "../../components/week10/MyMapView";
+
+export default function LocationQuiz() {
+    const [location, setLocation] = useState(null);
+
+    const onLoad = async () => {
+        let loc = await GPS.getLocation();
+        if (loc) {
+            //console.log("Location : ", loc);
+            setLocation(loc);
+        }
+    };
+    useEffect(() => {
+        console.log("ENTER SCREEN");
+        onLoad();
+    }, []);
+
+
+    //TOP RIGHT MENU
+    const navigation = useNavigation();
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => (
+                <TouchableOpacity
+                    style={{ paddingRight: 20 }}
+                    onPress={() => { navigation.navigate("LocationQuiz"); }}
+                >
+                    <Text>Quiz</Text>
+                </TouchableOpacity>
+            ),
+        });
+    }, [navigation]);
+    //SCREEN UI
+    return (
+        <View style={{ flex: 1 }}>
+            <Text>{location ? new Date(location.timestamp).toString() : "-"}</Text>
+            <View style={{ flexDirection: 'row', height: 70, backgroundColor: "#50E3C2" }}>
+            </View>
+
+            <View style={{ flex: 1 }}>
+                <MyMapView location={location}
+                    setLocation={setLocation} />
+            </View>
+        </View>
+
+    );
+}
